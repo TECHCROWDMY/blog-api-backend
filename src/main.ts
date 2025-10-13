f3ec7fa6edfcca3609ccdcf006d43f75489cd7f0
Thilak Sundaram <techcrowdmy@gmail.com>
2025-10-13 23:47:07 +0800

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    
+  // 💡 ADD THIS LINE TO ENABLE CORS
+  app.enableCors({
+    origin: true, // Allows all origins, or set to a specific array of domains
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Allowed methods
+    credentials: true, // Allows cookies/authorization headers
+  }); 
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
